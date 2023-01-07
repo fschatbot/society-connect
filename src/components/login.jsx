fc8dd44639f0de41d2a3d13577856ddaf4eef8c9
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-01-07 16:02:02 +0530

    Completed the login Page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,37 @@
+import { useRef } from "react";
 import "../styles/login.css";
+import logo from "./assets/logo.png";
 
 function Login() {
-	return <>Login</>;
+	let inpRef = useRef(null);
+	let passRef = useRef(null);
+
+	return (
+		<div className="page">
+			<div className="formBox">
+				<img src={logo} className="h-20" alt="Page Logo" />
+				<h1 className="text-4xl font-bold">Society Connect</h1>
+				<div className="inputContainer">
+					<div className="inputWrapper">
+						<input ref={inpRef} placeholder="Ex: B404" />
+						<label>Flat No</label>
+					</div>
+					<p className="error">Invalid building Name</p>
+				</div>
+				<div className="inputContainer">
+					<div className="inputWrapper">
+						<input id="password" ref={passRef} placeholder=" " type="password" />
+						<label htmlFor="password">Password</label>
+					</div>
+					<p className="error">Invalid Password</p>
+				</div>
+				<div className="w-full flex justify-end">
+					<button>Log In</button>
+				</div>
+			</div>
+			<p>In case of queries regarding log in details please approch the moderator in B-404</p>
+		</div>
+	);
 }
 
 export default Login;
