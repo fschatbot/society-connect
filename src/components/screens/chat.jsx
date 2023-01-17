a85522d009b9be5773a1afb49890e85b7626de1d
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-01-17 22:21:55 +0530

    Completed the chat layout page

--- src/components/screens/chat.jsx
+++ src/components/screens/chat.jsx
@@ -1,7 +1,61 @@
 import "../../styles/chat.css";
+import { Icon } from "@iconify/react";
+import { useEffect, useState } from "react";
+import { Get } from "../../firebase";
+import { Link } from "react-router-dom";
 
 function Chat() {
-	return <>Chat</>;
+	return (
+		<>
+			<nav className="ChatNav">
+				<h1>Messages</h1>
+			</nav>
+			<div className="searchBox">
+				<input className="search" type="text" placeholder="Search..." />
+				<Icon icon="uil:search" />
+			</div>
+			<div className="chatList">
+				<Link className="bigChat" to={"/chat/society"}>
+					<div className="chatData">
+						<Icon icon="lucide:building-2" />
+						Society Chat
+					</div>
+					<Icon icon="ic:round-keyboard-arrow-right" />
+				</Link>
+				<Link className="bigChat" to={"/chat/building"}>
+					<div className="chatData">
+						<Icon icon="lucide:building" />
+						Building Chat
+					</div>
+					<Icon icon="ic:round-keyboard-arrow-right" />
+				</Link>
+				{ChatList()}
+			</div>
+		</>
+	);
+}
+
+function ChatList() {
+	const [accounts, setAccounts] = useState([]);
+
+	useEffect(() => {
+		Get("accounts").then((snap) => setAccounts(Object.values(snap.val())));
+	}, []);
+
+	return accounts.map((account) => {
+		return (
+			<Link to={`/chat/${account.username}`} className="chat" key={account.username}>
+				<div className="chatData">
+					<img className="PFP" src={account.PFP} />
+					<div className="nameWrapper">
+						<h1>{account.username}</h1>
+						<h2>Last Message...</h2>
+					</div>
+				</div>
+				<Icon icon="ic:round-keyboard-arrow-right" />
+			</Link>
+		);
+	});
 }
 
 export default Chat;
