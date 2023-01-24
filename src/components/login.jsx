84e0c7856d81b13aff0d822e412cb248519e383a
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-01-24 20:57:17 +0530

    Added dark mode for the login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -43,7 +43,7 @@ function Login() {
 	return (
 		<div className="login_page" style={{ height: "100%" }}>
 			<div className="formBox">
-				<img src={process.env.PUBLIC_URL + "/logo.png"} className="h-20" alt="Page Logo" />
+				<img src={process.env.PUBLIC_URL + (window.matchMedia("(prefers-color-scheme: dark)").matches ? "/logoDark.png" : "/logo.png")} className="h-20" alt="Page Logo" />
 				<h1 className="text-4xl font-bold">Society Connect</h1>
 				<div className="inputContainer">
 					<div className="inputWrapper">
