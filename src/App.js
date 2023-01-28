56eef5b11fc4cb5c0fa06e3f82c45a436e472437
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-01-28 13:33:02 +0530

    Enabled Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import "./App.css";
 import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import Navbar from "./components/navbar";
-import { Chat, Gossip, Login, Profile, ChatPage } from "./screens";
 import { isLoggedIn } from "./firebase";
 import { useEffect } from "react";
 
+// Importing Screen Elements
+import { Suspense, lazy } from "react";
+import HashLoader from "react-spinners/HashLoader";
+const Chat = lazy(() => import("./components/screens/chat"));
+const Gossip = lazy(() => import("./components/screens/gossip"));
+const Login = lazy(() => import("./components/login"));
+const Profile = lazy(() => import("./components/screens/profile"));
+const ChatPage = lazy(() => import("./components/screens/chatPage"));
+
 function App() {
 	return (
-		<Routes>
-			<Route path="/login" element={<Login />} />
-			<Route
-				path="/gossip"
-				element={
-					<PageWrapper>
-						<Gossip />
-					</PageWrapper>
-				}
-			/>
-			<Route path="/chat/:id" element={<ChatPage />} />
-			<Route
-				path="/chat"
-				element={
-					<PageWrapper>
-						<Chat />
-					</PageWrapper>
-				}
-			/>
-			<Route
-				path="/profile/:id?"
-				element={
-					<PageWrapper>
-						<Profile />
-					</PageWrapper>
-				}
-			/>
-			<Route path="*" element={<Navigate to="/login" replace />} />
-		</Routes>
+		<Suspense
+			fallback={
+				<div className="loadingScreen">
+					<HashLoader color="#36d7b7" />
+				</div>
+			}>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route
+					path="/gossip"
+					element={
+						<PageWrapper>
+							<Gossip />
+						</PageWrapper>
+					}
+				/>
+				<Route path="/chat/:id" element={<ChatPage />} />
+				<Route
+					path="/chat"
+					element={
+						<PageWrapper>
+							<Chat />
+						</PageWrapper>
+					}
+				/>
+				<Route
+					path="/profile/:id?"
+					element={
+						<PageWrapper>
+							<Profile />
+						</PageWrapper>
+					}
+				/>
+				<Route path="*" element={<Navigate to="/login" replace />} />
+			</Routes>
+		</Suspense>
 	);
 }
 
--- src/screens.js
+++
@@ -1,7 +0,0 @@
-import Chat from "./components/screens/chat";
-import Gossip from "./components/screens/gossip";
-import Login from "./components/login";
-import Profile from "./components/screens/profile";
-import ChatPage from "./components/screens/chatPage";
-
-export { Chat, Gossip, Login, Profile, ChatPage };
